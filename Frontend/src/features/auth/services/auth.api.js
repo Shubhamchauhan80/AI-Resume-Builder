ec2488f477871a66a26bb22f34dcf4ab3ac83da8
Shubham Kumar <sk1314518@gmail.com>
2026-08-05 16:29:01 +0530

    Update auth.api.js

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-resume-builder-envr.onrender.com",
     withCredentials: true
 })
 
