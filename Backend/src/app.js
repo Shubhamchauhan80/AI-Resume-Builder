9ce54cf08208850d49e1d4a13c1f06af89405c4b
Shubham Kumar <sk1314518@gmail.com>
2026-08-12 21:28:51 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
     origin: "https://ai-resume-builder-frontend-gbpo.onrender.com",
     credentials: true
